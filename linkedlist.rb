5da4451829a53f3a482829c9c8055ed1a5247191
weeezik <mkihanya.33@gmail.com>
2024-11-25 11:43:44 -0800

    First draft append method

--- linkedlist.rb
+++ linkedlist.rb
@@ -6,15 +6,32 @@ class LinkedList
     @head = Node.new("head", @tail)
   end
 
-  def append value
+  def append value #add node with given value to the end of the list
+    current_node = @head
+    if current_node.next_node != @tail
+      current_node = current_node.next_node
+    else
+      current_node.next_node = Node.new(value, @tail)
+    end
   end
 
   def to_s
-    puts "[#{@head.value}]->[#{@head.next_node.value}]->[#{@tail.value}]"
+    each_node = []
+    each_node << @head
+    until each_node[-1] == @tail
+      each_node << each_node[-1].next_node
+    end
+
+    each_nodes_value = []
+    each_node.each do |node|
+      each_nodes_value << node.value
+    end
+    each_nodes_value.join(" -> ")
   end
 
 end
 
 the_list = LinkedList.new
-
+puts the_list
+the_list.append('dog')
 puts the_list
