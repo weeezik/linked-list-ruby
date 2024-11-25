12f209a97b501e07d063aea90197f7a0d916aa2f
weeezik <mkihanya.33@gmail.com>
2024-11-25 11:23:27 -0800

    New restart point
    From...whiteboard session in REI when the power
    was out

--- linkedlist.rb
+++ linkedlist.rb
@@ -1,24 +1,12 @@
 require_relative 'node'
 
 class LinkedList
-
   def initialize
     @tail = Node.new("tail", nil)
     @head = Node.new("head", @tail)
   end
 
   def append value
-    # While the current node's next node is not the tail node, set the current node
-    # to the next node in the list.
-    # Once the current node's next node is the tail node, # set that current node's, 
-    # next node to a node with the value given and that points to the @tail node.
-    added_node = Node.new(value, @tail)
-
-    @head.next_node = added_node
-    
-  end
-  
-  def prepend value
   end
 
   def to_s
@@ -27,10 +15,6 @@ class LinkedList
 
 end
 
+the_list = LinkedList.new
 
-
-linkedlist = LinkedList.new
-
-linkedlist.append("daniel")
-
-puts linkedlist
+puts the_list
