4e112e0d6ad4837c8fc34c23e730288fdfd083c1
weeezik <mkihanya.33@gmail.com>
2024-11-26 12:30:48 -0800

    Add main.rb file to test methods

--- linkedlist.rb
+++ linkedlist.rb
@@ -128,18 +128,19 @@ class LinkedList
 
 end
 
-the_list = LinkedList.new
-the_list.append('dog')
-the_list.prepend('cat')
-the_list.prepend('meow')
-the_list.prepend('red')
-the_list.append('cow')
-puts the_list
-the_list.size
-the_list.head
-the_list.tail
-the_list.at(1)
-the_list.pop
-puts the_list
-puts the_list.contains? "cat"
-p the_list.find("meow")
+# the_list = LinkedList.new
+# the_list.append('dog')
+# the_list.prepend('cat')
+# the_list.prepend('meow')
+# the_list.prepend('red')
+# the_list.append('cow')
+# puts the_list
+# the_list.size
+# the_list.head
+# the_list.tail
+# the_list.at(1)
+# the_list.pop
+# puts the_list
+# puts the_list.contains? "cat"
+# p the_list.find("meow")
+# 
---
+++ main.rb
@@ -0,0 +1,12 @@
+require_relative 'linkedlist'
+
+list = LinkedList.new
+
+list.append('dog')
+list.append('cat')
+list.append('parrot')
+list.append('hamster')
+list.append('snake')
+list.append('turtle')
+
+puts list
