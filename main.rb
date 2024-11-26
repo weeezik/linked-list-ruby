15f4deecf27eb4cb28780d8c0164e43c3262f40b
weeezik <mkihanya.33@gmail.com>
2024-11-26 12:32:16 -0800

    Move tests from class file to main

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require_relative 'linkedlist'
 
+# TOP-suggested tests
 list = LinkedList.new
 
 list.append('dog')
@@ -8,5 +9,23 @@ list.append('parrot')
 list.append('hamster')
 list.append('snake')
 list.append('turtle')
-
 puts list
+
+# My Tests
+the_list = LinkedList.new
+
+the_list.append('dog')
+the_list.prepend('cat')
+the_list.prepend('meow')
+the_list.prepend('red')
+the_list.append('cow')
+puts the_list
+
+the_list.size
+the_list.head
+the_list.tail
+the_list.at(1)
+the_list.pop
+puts the_list
+puts the_list.contains? "cat"
+p the_list.find("meow")
